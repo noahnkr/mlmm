24d1d38697016410f7ed3b1e34060cabed91d00c
Noah Roberts <noahkrob97@gmail.com>
2025-04-07 13:46:21 -0500

    implemented basic stats collection

---
+++ scraper/basic_stats.py
@@ -0,0 +1,42 @@
+import requests
+import csv
+import pandas as pd
+from bs4 import BeautifulSoup 
+from utils import (
+    BASIC_TEAM_STATS, ADVANCED_TEAM_STATS,
+)
+
+matchups = pd.read_csv("data/matchups.csv")
+seen = set()
+
+basic_team_stats = []
+
+for _, matchup in matchups.iterrows():
+    for team_col in ["team_a", "team_b"]:
+        year = matchup["year"]
+        team = matchup[team_col]
+        team_link = matchup[f"{team_col}_link"]
+        key = (year, team)
+
+        if key in seen: continue # We have already collected tihs teams stats
+        seen.add(key)
+        print(f"--- Scraping {year} {team} Basic Stats ---")
+
+        res = requests.get(team_link)
+        soup = BeautifulSoup(res.content, "lxml")
+
+        stats_table = soup.find("table", {"id": "season-total_per_game"})
+        basic_stats = { "year": year, "team": team }
+        for stat_name, stat_id in BASIC_TEAM_STATS.items():
+            stat = float(
+                stats_table.find("td", {"data-stat": stat_id}).text.strip()
+            )
+            basic_stats[stat_name] = stat
+
+        print(basic_stats)
+        basic_team_stats.append(basic_stats)
+
+with open("data/basic_stats.csv", "w", newline="", encoding="utf-8") as f:
+    writer = csv.DictWriter(f, fieldnames=basic_team_stats[0].keys())
+    writer.writeheader()
+    writer.writerows(basic_team_stats)
