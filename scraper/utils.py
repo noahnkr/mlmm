a3e4cd2d4c7152366eb177ace68257c673e5fa10
Noah Roberts <noahkrob97@gmail.com>
2025-04-07 13:30:28 -0500

    Mapped stat names to respective ids

--- scraper/utils.py
+++ scraper/utils.py
@@ -6,6 +6,42 @@ REGIONS = [ "East", "Midwest", "South", "West", "National" ]
 
 ROUNDS = ["First", "Second", "Sweet 16", "Elite 8", "Final 4", "National Championship"]
 
+BASIC_TEAM_STATS = {
+    "2P": "fg2_per_g", 
+    "2PA": "fg2a_per_g", 
+    "3P": "fg3_per_g", 
+    "3PA": "fg3a_per_g", 
+    "FT": "ft_per_g", 
+    "FTA": "fta_per_g", 
+    "ORB": "orb_per_g", 
+    "DRB": "drb_per_g", 
+    "AST": "ast_per_g", 
+    "STL": "stl_per_g", 
+    "BLK": "blk_per_g", 
+    "TOV": "tov_per_g", 
+    "PF": "pf_per_g", 
+    "PTS": "pts_per_g",
+}
+
+ADVANCED_TEAM_STATS = {
+   "W-L%": "win_loss_pct", 
+   "SRS": "srs",
+   "SOS": "sos", 
+   "Pace": "pace",
+   "ORtg": "off_rtg",
+   "FTr": "fta_per_fga_pct",
+   "3PAr": "fg3a_per_fga_pct",
+   "TS%": "ts_pct",
+   "TRB%": "trb_pct",
+   "AST%": "ast_pct",
+   "STL%": "stl_pct",
+   "BLK%": "blk_pct",
+   "eFG%": "efg_pct",
+   "TOV%": "tov_pct",
+   "ORB%": "orb_pct",
+   "FT/FGA": "ft_rate",
+}
+
 UNDERLINE_START = "\033[4m"
 
 UNDERLINE_END = "\033[0m"
