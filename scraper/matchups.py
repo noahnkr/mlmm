8d7b162a471aa7304ce0009595a62c3692bff9af
Noah Roberts <noahnkr@iastate.edu>
2025-04-08 11:56:13 -0500

    Added request headers

--- scraper/matchups.py
+++ scraper/matchups.py
@@ -1,56 +1,57 @@
-import requests
+import time
+import random
 import csv
+import requests
 from bs4 import BeautifulSoup 
 from utils import (
-    YEARS, REGIONS, ROUNDS, BASE_URL, 
-    get_bracket_url, print_matchup
+	YEARS, REGIONS, ROUNDS, HEADERS,
+	get_bracket_url, print_matchup
 )
-
 
 matchup_history = []
 
 for year in YEARS:
-    print(f"--- Scraping {year} Season ---")
-    bracket_url = get_bracket_url(year)
-    res = requests.get(bracket_url)
-    soup = BeautifulSoup(res.content, "lxml")
+	print(f"--- Scraping {year} Season ---")
+	bracket_url = get_bracket_url(year)
+	res = requests.get(bracket_url, headers=HEADERS)
+	time.sleep(random.uniform(2.0, 5.0)) # Avoid rate limiting
+	soup = BeautifulSoup(res.content, "lxml")
 
-    for region in REGIONS:
-        print(f"--- Scraping {region} Region ---")
-        bracket = soup.find("div", {"id": region.lower()}) # Region id is lowercase
+	for region in REGIONS:
+		print(f"--- Scraping {region} Region ---")
+		bracket = soup.find("div", {"id": region.lower()}) # Region id is lowercase
 
-        rounds = bracket.find_all("div", {"class": "round"})[:-1] # Remove last round, which is just the region winner
-        bracket_rounds = ROUNDS[:-2] if region != "National" else ROUNDS[-2:]
-        for round, round_name in zip(rounds, bracket_rounds):
-            print(f"--- Scraping {round_name} Round ---")
+		rounds = bracket.find_all("div", {"class": "round"})[:-1] # Remove last round, which is just the region winner
+		bracket_rounds = ROUNDS[:-2] if region != "National" else ROUNDS[-2:]
+		for round, round_name in zip(rounds, bracket_rounds):
+			print(f"--- Scraping {round_name} Round ---")
 
-            matchups = round.find_all("div")
-            for matchup in matchups:
-                teams = matchup.find_all("div")
-                if len(teams) != 2: continue
+			matchups = round.find_all("div")
+			for matchup in matchups:
+				teams = matchup.find_all("div")
+				if len(teams) != 2: continue
 
-                team_a = teams[0].find("a").text.strip()
-                team_b = teams[1].find("a").text.strip()
+				team_a = teams[0].find("a").text.strip()
+				team_b = teams[1].find("a").text.strip()
 
-                team_a_seed = int(teams[0].find("span").text.strip())
-                team_b_seed = int(teams[1].find("span").text.strip())
+				team_a_seed = int(teams[0].find("span").text.strip())
+				team_b_seed = int(teams[1].find("span").text.strip())
 
-                winner = 0 if "winner" in teams[0].get("class", []) else 1
+				winner = 0 if "winner" in teams[0].get("class", []) else 1
 
-                print_matchup(team_a, team_b, team_a_seed, team_b_seed, winner)
-                matchup_history.append({
-                    "year": year,
-                    "region": region,
-                    "round": round_name,
-                    "team_a": team_a,
-                    "team_b": team_b,
-                    "team_a_seed": team_a_seed,
-                    "team_b_seed": team_b_seed,
-                    "winner": winner, # 0 if Team A wins, 1 if Team B wins
-                })
-
+				print_matchup(team_a, team_b, team_a_seed, team_b_seed, winner)
+				matchup_history.append({
+					"year": year,
+					"region": region,
+					"round": round_name,
+					"team_a": team_a,
+					"team_b": team_b,
+					"team_a_seed": team_a_seed,
+					"team_b_seed": team_b_seed,
+					"winner": winner, # 0 if Team A wins, 1 if Team B wins
+				})
 
 with open("data/matchups.csv", "w", newline="", encoding="utf-8") as f:
-    writer = csv.DictWriter(f, fieldnames=matchup_history[0].keys())
-    writer.writeheader()
-    writer.writerows(matchup_history)
+	writer = csv.DictWriter(f, fieldnames=matchup_history[0].keys())
+	writer.writeheader()
+	writer.writerows(matchup_history)
