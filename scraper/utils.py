126c737e896941d9626eff3112ce37ba1cb60ec3
Noah Roberts <noahnkr@iastate.edu>
2025-04-16 18:24:02 -0500

    Removed tournament games from stat collection data

--- scraper/utils.py
+++ scraper/utils.py
@@ -6,19 +6,17 @@ REGIONS = [ "East", "Midwest", "South", "West", "National" ]
 
 ROUNDS = [ "First", "Second", "Sweet 16", "Elite 8", "Final 4", "National Championship" ]
 
-BASIC_TEAM_STATS = {
-    "GP": "g",
-    "W-L%": "win_loss_pct", 
+NUMERIC_STATS = {
     "FG": "fg",
     "FGA": "fga",
-    "FG%": "fg_pct",
     "3P": "fg3",
     "3PA": "fg3a",
-    "3P%": "fg3_pct",
+    "2P": "fg2",
+    "2PA": "fg2a",
     "FT": "ft",
     "FTA": "fta",
-    "FT%": "ft_pct",
     "ORB": "orb",
+    "DRB": "drb",
     "TRB": "trb",
     "AST": "ast",
     "STL": "stl",
@@ -27,22 +25,12 @@ BASIC_TEAM_STATS = {
     "PF": "pf", 
 }
 
-ADVANCED_TEAM_STATS = {
-   "SRS": "srs",
-   "SOS": "sos", 
-   "Pace": "pace",
-   "ORtg": "off_rtg",
-   "FTr": "fta_per_fga_pct",
-   "3PAr": "fg3a_per_fga_pct",
-   "TS%": "ts_pct",
-   "TRB%": "trb_pct",
-   "AST%": "ast_pct",
-   "STL%": "stl_pct",
-   "BLK%": "blk_pct",
-   "eFG%": "efg_pct",
-   "TOV%": "tov_pct",
-   "ORB%": "orb_pct",
-   "FT/FGA": "ft_rate",
+PERCENT_STATS = {
+    "FG%": "fg_pct",
+    "3P%": "fg3_pct",
+    "2P%": "fg2_pct",
+    "eFG%": "efg_pct",
+    "FT%": "ft_pct",
 }
 
 HEADERS = {
@@ -53,11 +41,32 @@ UNDERLINE_START = "\033[4m"
 
 UNDERLINE_END = "\033[0m"
 
+STATS_OUTPUT_PATH = "data/stats.csv"
+
+MATCHUPS_OUTPUT_PATH = "data/matchups.csv"
+
+def collect_tournament_teams(matchups):
+    tournament_teams = []
+    seen = set()
+    for _, matchup in matchups.iterrows():
+        for team_col in ["team_a", "team_b"]:
+            year = matchup["year"]
+            team = matchup[team_col]
+
+            if (year, team) in seen:
+                continue
+
+            # Collects list of each tournament team for each year
+            tournament_teams.append((year, team))
+            seen.add((year, team))
+    
+    return tournament_teams
+
 def get_bracket_url(year):
     return f"{BASE_URL}/cbb/postseason/men/{year}-ncaa.html"
 
-def get_season_stats_url(year, basic_stats):
-    return f"{BASE_URL}/cbb/seasons/men/{year}-{"" if basic_stats else "advanced-"}school-stats.html"
+def get_season_stats_url(year, team):
+    return f"{BASE_URL}/cbb/schools/{team}/men/{year}-gamelogs.html"
 
 def print_matchup(team_a, team_b, team_a_seed, team_b_seed, winner):
    print(f"({team_a_seed}) {UNDERLINE_START if winner == team_a else ""}{team_a}{UNDERLINE_END if winner == team_a else ""} vs. ({team_b_seed}) {UNDERLINE_START if winner == team_b else ""}{team_b}{UNDERLINE_END if winner == team_b else ""}")
